e8a1175b520a6bd3b22521f7ff6bb2c52cd8b554
Seongmin Oh <saogig0503@naver.com>
2026-02-07 09:42:52 +0900

    1대1 관계 구현해보기

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -2,12 +2,16 @@ import { Controller, Get, Param, Patch, Post } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserModel } from './entity/user.entity';
 import { Repository } from 'typeorm';
+import { ProfileModel } from './entity/profile.entity';
 
 @Controller()
 export class AppController {
   constructor(
     @InjectRepository(UserModel)
     private readonly userRepository: Repository<UserModel>,
+
+    @InjectRepository(ProfileModel)
+    private readonly profileRepository: Repository<ProfileModel>,
   ) {}
 
   @Post('users')
@@ -19,7 +23,11 @@ export class AppController {
 
   @Get('users')
   getUsers() {
-    return this.userRepository.find();
+    return this.userRepository.find({
+      relations: {
+        profile: true,
+      },
+    });
   }
 
   @Patch('users/:id')
@@ -32,7 +40,20 @@ export class AppController {
 
     return this.userRepository.save({
       ...user,
-      title: user.title + '0',
     });
+  }
+
+  @Post('users/profile')
+  async createUserAndProfile() {
+    const user = await this.userRepository.save({
+      email: 'example@ex.com',
+    });
+
+    await this.profileRepository.save({
+      profileImg: 'asdf.jpg',
+      user,
+    });
+
+    return user;
   }
 }
